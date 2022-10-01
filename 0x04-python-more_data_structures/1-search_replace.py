cd4b63f0124a622211468c9c1536080cf451892a
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-01 12:49:48 -0700

    Adding find replace function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-search_replace(my_list, search, replace):
+def search_replace(my_list, search, replace):
     """replaces an element search from a list by replace"""
     newList = list(my_list)
     for i in range(len(newList)):
