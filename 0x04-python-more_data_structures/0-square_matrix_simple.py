258e427f1a9d1cc4b13c3ae13100916544c23ed6
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-01 11:57:05 -0700

    Adding function to calculate sqaure of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+def square_matrix_simple(matrix=[]):
+    """function that computes the square value of all integers of a matrix."""
+    new_matrix = []
+    for i in matrix:
+        mat = []
+        for j in range(len(i)):
+            sq = i[j] ** 2
+            mat.append(sq)
+        new_matrix.append(mat)
+    return new_matrix
