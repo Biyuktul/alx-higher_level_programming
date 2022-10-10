da53b6c70934930e417422bbc142e3df700aba45
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-10 12:15:11 -0700

    Adding file

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        result = int(value)
+    except TypeError:
+        return (False)
+    else:
+        print("{:d}".format(result))
+        return (True)
