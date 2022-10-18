3c23ab5ba93760af60fde60386158d01ce68e6fe
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-17 20:03:45 -0700

    Bug fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,10 +8,11 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-    """Getter"""
+    @width.property
     def width(self):
         return (self.width)
-    """Setter"""
+
+    @width.setter
     def width(self, value):
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -19,11 +20,12 @@ class Rectangle:
             self.width = value
         else:
             raise TypeError("width must be an integer")
-    """Getter"""
+
+    @height.property
     def height(self):
         return (self.height)
 
-    """Setter"""
+    @height.setter
     def height(self, value):
         if value < 0:
             raise ValueError("height must be >= 0")
