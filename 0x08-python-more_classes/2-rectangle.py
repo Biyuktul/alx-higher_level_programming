f8e1cc6213c165356021bd99b58a9262be547531
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-18 08:44:33 -0700

    Adding file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,6 @@ class Rectangle:
 
     def perimeter(self):
         """perimeter calculator"""
+        if (self.__width == 0 or slef.__height == 0):
+            return 0
         return (self.__width + self.__height) * 2
