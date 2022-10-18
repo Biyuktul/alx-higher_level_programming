78fa701a3d4191b2fcfb51e1f52d31ca98505563
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-17 19:34:35 -0700

    Adding files

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,14 +3,15 @@
 
 
 class Rectangle:
-
+    """Rectangle class with 2 private instance members"""
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
+    """Getter"""
     def width(self):
         return (self.width)
-
+    """Setter"""
     def width(self, value):
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -18,10 +19,11 @@ class Rectangle:
             self.width = value
         else:
             raise TypeError("width must be an integer")
-
+    """Getter"""
     def height(self):
         return (self.height)
 
+    """Setter"""
     def height(self, value):
         if value < 0:
             raise ValueError("height must be >= 0")
