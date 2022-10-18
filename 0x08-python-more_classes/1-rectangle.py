ecb8f6c0514710f90fd642c6e9d164e844ae7c8e
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-10-17 20:25:30 -0700

    Bug fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python3
-"""Rectangle Class"""
+"""Rectangle Class Definition"""
 
 
 class Rectangle:
     """Rectangle class with 2 private instance members"""
     def __init__(self, width=0, height=0):
-        """Initilizing"""
-        self.width(width)
-        self.height(height)
+        """initializer"""
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if (isinstance(width, int)):
+            self.__width = width
+        else:
+            raise TypeError("width must be an integer")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if (isinstance(height, int)):
+            self.__height = height
+        else:
+            raise TypeError("height must be an integer")
 
     @property
     def width(self):
-        """Returns width instance variable"""
-        return (self.width)
+        """Getter"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """sets value to width instance varible"""
+        """Width Setter"""
         if value < 0:
             raise ValueError("width must be >= 0")
         if (isinstance(value, int)):
-            self.width = value
+            self.__width = value
         else:
             raise TypeError("width must be an integer")
 
     @property
     def height(self):
-        """Returns width instance variable"""
-        return (self.height)
+        """Height getter"""
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        """Sets value to height instance variable"""
+        """height setter"""
         if value < 0:
             raise ValueError("height must be >= 0")
-        if (isinstance(int, value)):
-            self.height = value
+        if (isinstance(value, int)):
+            self.__height = value
         else:
             raise TypeError("height must be an integer")
