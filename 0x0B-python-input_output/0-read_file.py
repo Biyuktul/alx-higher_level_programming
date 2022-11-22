9c2f942210c3ebd295d9595bd024de8df301739c
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-22 15:28:24 +0300

    Adding file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a program to read and print utf-8 textfile """
+
+
+def read_file(filename=""):
+    """
+    function to read file pointed by filename
+    """
+    with open(filename, 'r') as file_content:
+        content = file_content.read()
+        print(content)
