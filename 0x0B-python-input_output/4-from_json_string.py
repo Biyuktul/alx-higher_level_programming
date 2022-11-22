51cfc1a3985e5f2c2d3c07c4bfc9f73aa965a3b0
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-22 16:10:30 +0300

    adding file

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a program that returns an object (Python data structure) represented by a JSON string: """
+import json
+
+
+def from_json_string(my_str):
+    """
+    function to return object representation of jason string
+    :param my_str: the json string
+    :return: object representation
+    """
+    return json.loads(my_str)
