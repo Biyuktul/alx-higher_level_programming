ffb4d3f7908a0b2af971c1e80c04efd2b4c82c8c
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-22 16:28:09 +0300

    adding file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -4,5 +4,9 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    creates a python object from json file
+    :param filename: the json file
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         return json.load(file)
