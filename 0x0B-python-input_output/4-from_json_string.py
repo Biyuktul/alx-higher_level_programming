7fbaab48c56460d015775aab4225a0024eea2405
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-22 16:12:21 +0300

    adding file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a program that returns an object (Python data structure) represented by a JSON string: """
+"""a program that returns an object (Python data structure)
+    represented by a JSON string: """
 import json
 
 
