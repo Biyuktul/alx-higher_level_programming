ed0a6548cdf722faed2e13c2398dc77d73d94440
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-11-29 23:22:21 -0800

    Adding file

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- selecting rows that are not NULL
-SELECT name, score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
