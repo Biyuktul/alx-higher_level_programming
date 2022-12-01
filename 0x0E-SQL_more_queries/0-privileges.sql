83df34049ad8aaab83e96f3a371749056c4b4b2a
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-12-01 11:04:47 -0800

    Adding file

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- listing all privileges of the MySQL users user_0d_1 and user_0d_2 on the server
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
