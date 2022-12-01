7c1896d96a04530127598a7e500a536b16e15f49
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-12-01 06:07:26 -0800

    Adding file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++
@@ -1,7 +0,0 @@
--- displaying the average temprature by city
--- ordered by temperature descending in mysql
-
-SELECT city, AVG(value) as avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
