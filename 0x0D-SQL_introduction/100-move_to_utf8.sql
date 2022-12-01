3ab0b4b58576ea1e60ccfb1b480a2abf6dfa73d5
Yonatan Addis <yonatanaddis0257@gmail.com>
2022-12-01 06:01:33 -0800

    Bug fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- converting hbtn_0c_0 database to UTF8 in mysql
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
