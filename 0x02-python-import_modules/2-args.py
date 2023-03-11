29f0d7a40bb67e5b11ba73bbee41fdf534a430db
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-11 09:40:26 +0300

    adding files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    arg_str = "arguments" if len(argv) > 1 else "argument"
+    print("{} {}:".format(len(argv) - 1, arg_str))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
