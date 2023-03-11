daff7a85706a3f0265b4e1e2c3842ff5420e98c3
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-11 09:53:17 +0300

    bug fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 
 if __name__ == "__main__":
-    arg_str = "argument" if len(argv) > 1 else "arguments"
+    arg_str = "arguments" if len(argv) != 2 else "argument"
     print("{} {}:".format(len(argv) - 1, arg_str))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
