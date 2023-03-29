e60f4ce78a0363754dc0b66a1c9572f91f918cd2
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-29 22:45:34 +0300

    adding file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+this module defines a square class
+"""
+
+
+class Square:
+    """
+    this class defines a Square blueprint
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        """
+        method to calculate area of the square
+        :return: area of the square
+        """
+        return self.__size * self.__size
