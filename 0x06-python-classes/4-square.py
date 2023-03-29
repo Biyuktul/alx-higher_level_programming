ba8025715f8fb7908a555e674f26602c22ce53a7
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-29 22:46:57 +0300

    adding file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,12 +9,7 @@ class Square:
     this class defines a Square blueprint
     """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     @property
     def size(self):
@@ -22,7 +17,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """
