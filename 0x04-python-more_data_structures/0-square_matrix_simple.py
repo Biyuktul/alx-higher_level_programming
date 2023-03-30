22293ebabd99e111a06f6753e15cc2f010935e00
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-03-30 23:03:41 +0300

    adding file

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = list(
+        map(lambda row: list(map(lambda element: element ** 2, row)), matrix)
+    )
+    return new_matrix
