7213bd71523f038c13a0ce0f71fe404d2951b2cf
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-04 09:45:19 +0300

    bug fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,5 +48,8 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        return 2(self.__width * self.__height) if self.__width != 0\
-                                                  or self.__height != 0 else 0
+        result = 0
+        if self.__width != 0 and self.__height != 0:
+            result = 2 * (self.__width + self.__height)
+
+        return result
