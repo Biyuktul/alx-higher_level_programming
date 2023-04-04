a9a63b7a110c180699d633207405388314126bca
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-04 09:07:04 +0300

    adding files

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,22 @@
 #!/usr/bin/pythoon3
 class Rectangle:
-    """a"""
+    """
+    Rectangle class contains two private instance properties
+    :width: width of the rectangle
+    :height: height of the rectangle
+    """
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
