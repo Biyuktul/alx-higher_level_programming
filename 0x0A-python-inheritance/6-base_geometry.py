f3c5a69664978f750dd355e1fe66e3ae2bd1588b
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-10 13:15:24 +0300

    modifying

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """defines area public instance method"""
+    def area(self):
+        """raises Exception"""
+        raise Exception("area() is not implemented")
