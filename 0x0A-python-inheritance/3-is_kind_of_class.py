695425295437138020d3b83aec576b647135b125
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-10 13:04:02 +0300

    modifying

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -9,12 +9,4 @@ def is_kind_of_class(obj, a_class):
     :param a_class: the class
     :return: True if obj is subclass of a_class, False otherwise
     """
-    return issubclass(obj.__class__, a_class)
-
-a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+    return isinstance(obj, a_class)
