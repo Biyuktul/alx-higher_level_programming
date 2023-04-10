830142a1767ad7b8816bafcbcfa325aeb785db6e
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-10 10:16:39 +0300

    adding file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a function to return all available\
+attributes and methods of an object"""
+
+
+def lookup(obj):
+    """
+    looks for available attributes of an object
+    :param obj: the object to look for
+    :return: a list of available attribute and methods
+    """
+    return dir(obj)
