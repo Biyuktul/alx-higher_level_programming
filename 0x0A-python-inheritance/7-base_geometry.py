8640424a2a318ed37d83033e9bfb350bd5836201
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-10 13:19:11 +0300

    modifying

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """defines area public instance method"""
+    def area(self):
+        """raises Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        :param name: the name
+        :param value: the value
+        :return: void
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
