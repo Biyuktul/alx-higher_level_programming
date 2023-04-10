ca7bd2c864e8388db1ca236ec00af88fbe9b1aca
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-10 12:30:22 +0300

    modifying

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,4 @@
 class MyList(list):
     def print_sorted(self):
         """prints a sorted version of given list"""
-        for i in self:
-            if not isinstance(i, int):
-                raise TypeError()
         print(sorted(self))
