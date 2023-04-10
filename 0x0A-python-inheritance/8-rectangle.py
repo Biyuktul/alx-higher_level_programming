993ff62c3a348c5b536e92c5b2caf2d9c73cf561
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-10 14:45:03 +0300

    modifying

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,28 +17,7 @@ class Rectangle(BaseGeometry):
     - integer_validator(self): Validates width and height of the rectangle
     """
     def __init__(self, width, height):
-        try:
-            self.integer_validator('width', width)
-            self.__width = width
-        except (ValueError, TypeError) as e:
-            raise e
-        try:
-            self.integer_validator('height', height)
-            self.__height = height
-        except (ValueError, TypeError) as e:
-            raise e
-
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
