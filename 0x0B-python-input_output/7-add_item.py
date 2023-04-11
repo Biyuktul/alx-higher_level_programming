e758d12db36ceb23eb65a90b76dbf3fd659a4277
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-11 18:28:46 +0300

    modifying

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,5 +7,4 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 new_list = []
 for i in range(1, len(sys.argv)):
     new_list.append(sys.argv[i])
-with open('add_item.json', 'w', encoding="UTF8") as file:
-    json.dump(new_list, file)
+save_to_json_file(new_list, "add_item.json")
