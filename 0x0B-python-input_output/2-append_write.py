63d58861bb2b779f5ea264441e920ab186fbe243
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-11 18:25:24 +0300

    modifying

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defines a function append_write that appends a string at the end of a text file"""
+"""defines a function append_write"""
 
 
 def append_write(filename="", text=""):
