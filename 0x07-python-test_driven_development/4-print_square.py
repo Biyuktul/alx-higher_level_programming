0db41f9ee3572502ce0e0f5ff5e35be0b0a301e6
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-04-12 10:56:33 +0300

    modifying

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define a function print_square"""
+
+
 def print_square(size):
+    """prints a square with the character #"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
