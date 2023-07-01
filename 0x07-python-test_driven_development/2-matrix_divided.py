62ac9019be8f483f39fd0f2a8d922b5b8dde0634
= <=>
2023-07-01 11:07:41 -0400

    refactoring

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,44 +2,46 @@
 """defines a function matrix_divided"""
 
 
+
 def matrix_divided(matrix, div):
-    """
-    a function that divides each element of a list
-    of list by div and return a new list of results
-    :param matrix: a list of list
-    :param div: a divider
-    :return: new list of list where each element rounded to 2 decimal places
-    """
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if is_list_of_lis_of_int_or_float(matrix):
-        new_list = []
-        if isinstance(div, (int, float)):
-            for i in matrix:
-                if len(i) != len(matrix[0]):
-                    raise TypeError("Each row of the matrix must "
-                                    "have the same size")
-                row = []
-                for j in i:
-                    row.append(round(j / div, 2))
-                new_list.append(row)
-        else:
-            raise TypeError("div must be a number")
-    return new_list
+  """
+  function that divides each element of a list
+  of list by div and return a new list of list of result
+  :param matrix: a list of list
+  :param div: a divider
+  :return: new list of list where each element rounded to 2 decimal places
+  """
 
+  if not (isinstance(matrix, list) and all(
+    [isinstance(x, list) and isinstance(y, (int, float)) for x in matrix for y in x])):
+    raise TypeError(
+      'matrix must be a matrix (list of lists) of integers/floats')
 
-def is_list_of_lis_of_int_or_float(x):
-    """
-    checks whether a variable is a list of lists of int or float
-    :param x: the variable tobe checked
-    :return: True if it is, otherwise False
-    """
-    if isinstance(x, list):
-        for i in x:
-            for j in i:
-                if not isinstance(j, (int, float)):
-                    raise TypeError(
-                        """matrix must be a matrix (list of lists) of integers/floats""")
-    else:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    return True
+  if not check_row_size(matrix):
+    raise TypeError('Each row of the matrix must have the same size')
+
+  if not isinstance(div, (int, float)):
+    raise TypeError('div must be a number')
+  if div == 0:
+      raise ZeroDivisionError('division by  zero')
+
+  new_matrix = []
+  for i in matrix:
+    row = []
+    for j in i:
+        row.append(round(j / 3, 2))
+    new_matrix.append(row)
+
+  return new_matrix
+
+def check_row_size(matrix):
+  """
+  a function to check row size
+  :param matrix: a list of list
+  :return: True if row size are equal, False otherwise
+  """
+  size = len(matrix[0])
+  for i in matrix:
+    if len(i) != size:
+      return False
+  return True
