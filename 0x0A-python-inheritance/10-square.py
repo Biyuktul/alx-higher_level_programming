1b81e2787179196d92774684aa3e3f79a335d921
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-07-13 09:32:56 +0300

    subclassing rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,15 +6,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """class to represent rectangle object"""
 
-    def __init__(self, width, height):
-        super().integer_validator("height", height)
-        super().integer_validator("width", width)
-        self.__width = width
-        self.__height = height
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
 
     def area(self):
         """method to calculate area of the rectangle"""
-        return (self.__width * self.__height)
+        return (self.__size * self.__size)
 
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return f"[Rectangle] {self.__size}/{self.__size}"
