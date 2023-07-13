6a03f2199f32180f75fe610974e12fb433387bca
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-07-13 10:34:52 +0300

    subclassing in and overiding __eq__ and __ne__ methods

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,10 +3,10 @@
 
 
 class MyInt(int):
-    """defines MyInt to represent int object"""
+    """represents integer object with -= and == operators reversed"""
 
     def __eq__(self, other):
         return not super().__eq__(other)
 
     def __ne__(self, other):
-        return not self.__eq__(other)
+        return super().__eq__(other)
