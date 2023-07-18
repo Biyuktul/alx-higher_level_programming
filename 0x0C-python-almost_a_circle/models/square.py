292fa6319e7fded7e9d270fd22041c64325b7639
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-07-18 13:40:39 +0300

    implementing square lass that inherits from rectangle class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/pyhton3
 """defines a square class"""
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
     """represents a square"""
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """string representation of the rectangle instance"""
+        """string representation of the square instance"""
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
 
     @property
@@ -38,3 +39,9 @@ class Square(Rectangle):
             if len(args) > 4:
                 self.y = args[4]
 
+    def to_dictionary(self):
+        """
+        method to represent the rectangle with dictionary
+        :return: dictionary repr of the square
+        """
+        return {'id': self.id, 'x': self.x, 'y': self.y, 'size': self.size}
