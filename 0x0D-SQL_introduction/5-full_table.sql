ea4c6a8c1daa2da1c141ac79a87de7dd01edd523
Yonatan Addis <yonatanaddis0257@gmail.com>
2023-08-16 11:21:37 +0300

    shows full description of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
--- creates a database if hbtn_0c_0 is not already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- shows full description of a table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
