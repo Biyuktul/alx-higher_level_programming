e8bb9290b1d009f8e2a0c0e8a56d3b0f1080e63e
yonatan addis <yonatanaddis0257@gmail.com>
2023-12-24 10:13:07 +0300

    adding a feature to retrive needed attributes only

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,14 +6,24 @@ class Student:
     """
     represents a student
     """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         returns a dictionary representation of a class Student
         """
+        if isinstance(attrs, list)\
+            and all(isinstance(item, str) for item in attrs):
+            result = {}
+            for k in attrs:
+                if hasattr(self, k):
+                    result[k] = getattr(self, k)
+            return result
+        # or a precise way to do the above is using the dict comprehention
+        # return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
 
         return self.__dict__
