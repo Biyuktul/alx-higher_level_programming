ab00c9ecc84802d284df47d07a4aa298e4bb7ea4
yonatan addis <yonatanaddis0257@gmail.com>
2023-12-25 18:09:48 +0300

    say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 --lists all records of the table second_table of the database hbtn_0c_0
 -- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC `name` DESC;
+SELECT score, name
+FROM second_table
+WHERE name != "" 
+ORDER BY score DESC, name DESC;
